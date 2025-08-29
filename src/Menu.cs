b84aa9ac386f6531eaf52a8e12c19b03efa9ffeb
Tachoviendo <ignacio.3249@gmail.com>
2025-08-29 20:43:25 -0300

    Quedó el menú brothers

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,13 +1,33 @@
+
+using System;
 using System.Collections.Generic;
 
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa el conjunto de platillos <see cref="Dish"/> disponibles en el
-    /// restaurante.
-    /// </summary>
     public class Menu
     {
         private List<Dish> dishes = new List<Dish>();
+
+        public void AddDish(Dish plato)
+        {
+            this.dishes.Add(plato);
+        }
+
+        public void RemoveDish(Dish plato)
+        {
+            this.dishes.Remove(plato);
+        }
+
+        public Dish GetDishByName(string nombre)
+        {
+            foreach (var plato in this.dishes)
+            {
+                if (plato.Name == nombre)
+                {
+                    return plato;
+                }
+            }
+            return null;        
+        }
     }
 }
